c16850d73f5259c9acd9e799c35a2e1182349c55
Yuzhi ZHAO <yzzhao2-c@my.cityu.edu.hk>
2021-10-03 19:04:10 +0800

    Add files via upload

--- sbatch_run.sh
+++ sbatch_run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 #MV2_USE_CUDA=1 MV2_ENABLE_AFFINITY=0 MV2_SMP_USE_CMA=0
 #SBATCH -p Pixel
-#SBATCH --gres=gpu:8
-#SBATCH --nodelist=HK-IDC2-10-1-75-59
+#SBATCH --gres=gpu:4
 #SBATCH --job-name=cvpr2020
 #SBATCH -o ./logs/%j.txt
 srun --mpi=pmi2 sh ./local_run.sh
